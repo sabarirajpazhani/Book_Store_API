6434ce20c15f0d5bdc4a4f535a4623c28a7da970
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-03-23 14:01:44 +0530

    Env variables

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express ()
+const dotenv = require('dotenv')
+const path = require('path')
+
+dotenv.config({path:path.join(__dirname,'config', 'config.env')})
 
 
-app.listen(8000, ()=>{
-    console.log("Server is Running")
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is Running on ${process.env.PORT} for ${process.env.NODE_ENV}`)
 })
